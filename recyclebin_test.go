c9e216228d3c1942bfab96a6214fbefead1d93e4
Nikola Trubitsyn <nikola.trubitsyn@gmail.com>
2018-06-28 19:28:59 +0300

    Add tests

--- recyclebin_test.go
+++ recyclebin_test.go
@@ -4,7 +4,51 @@
 
 package recyclebin
 
-import "testing"
+import (
+	"testing"
+	"github.com/spf13/afero"
+	"os"
+)
 
-func TestSomething() {
+var fs = afero.NewMemMapFs()
+
+func TestMoveToTrash(t *testing.T) {
+	path := "/home/user/file"
+	trashPath := "/home/user/.local/share/Trash"
+	initHomeEnvironment()
+	MoveToTrash("/home/user/file")
+
+	success := !exists(path) && exists(trashPath+"/files/file") && exists(trashPath+"/info/file.trashinfo")
+	if !success {
+		t.Error("file has not been moved to trash")
+	}
+}
+
+func TestDeleteFromTrash(t *testing.T) {
+	initHomeEnvironment()
+	trashPath := "/home/user/.local/share/Trash"
+	fs.MkdirAll(trashPath+"/files", os.ModeDir)
+	fs.Create(trashPath + "/files/file")
+	fs.MkdirAll(trashPath+"/info", os.ModeDir)
+	fs.Create(trashPath + "/info/file.trashinfo")
+	DeleteFromTrash("file")
+
+	success := !exists(trashPath + "/files/file") && !exists(trashPath + "/info/file.trashinfo")
+	if !success {
+		t.Error("file has not been deleted from trash")
+	}
+}
+
+func TestRestoreFromTrash(t *testing.T) {}
+
+func TestEmptyTrash(t *testing.T) {}
+
+func initHomeEnvironment() {
+	fs.MkdirAll("/home/user/.local/share/Trash", os.ModeDir)
+	fs.Create("/home/user/file")
+}
+
+func exists(path string) bool {
+	dir, err := fs.Stat(path)
+	return err == nil && dir.Mode().IsDir()
 }
